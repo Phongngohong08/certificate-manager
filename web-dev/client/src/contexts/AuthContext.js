187bba685758888ff24b31deecd55bd3845812ed
Phongngohong08 <CT070337@actvn.edu.vn>
2025-05-24 21:54:21 +0700

    ok

--- web-dev/client/src/contexts/AuthContext.js
+++ web-dev/client/src/contexts/AuthContext.js
@@ -1,7 +1,7 @@
 import React, { createContext, useState, useContext, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 
-const API_URL = 'http://localhost:5000';
+const API_URL = 'http://localhost:3002';
 
 const AuthContext = createContext(null);
 
